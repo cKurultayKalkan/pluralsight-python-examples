46a0818f6f0294981d56aa6afeee8b304f09503d
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-09 18:04:40 +0300

    initial

---
+++ airtravel.py
@@ -0,0 +1,60 @@
+""""Model for aircraft  flights"""
+
+
+class Flight:
+    """A fligth with particular passenger aircraft."""
+
+    def __init__(self, number, aircraft):
+        if not number[:2].isalpha():
+            raise ValueError("No airline code in '{}'".format(number))
+
+        if not number[:2].isupper():
+            raise ValueError("Invalid Airline Code '{}'".format(number))
+
+        if not (number[2:].isdigit() and int(number[2:]) <= 9999):
+            raise ValueError("Invalid Route Number '{}'".format(number))
+
+        self._number = number
+        self._aircraft = aircraft
+
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [None] + [{letter: None for letter in seats} for _ in rows]
+
+    def number(self):
+        return self._number
+
+    def airline(self):
+        return self._number[:2]
+
+    def aircraft_model(self):
+        return self._aircraft.model()
+
+    def allocate_seat(self, passenger):
+        """Allocate a seat to a passanger.
+
+        Args:
+            seat: A seat designator such as '12C' or '21F'
+            passenger: The passanger name.
+
+        Raises:
+
+        """
+
+
+
+class Aircraft:
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+
+    def registration(self):
+        return self._registration
+
+    def model(self):
+        return self._model
+
+    def seating_plan(self):
+        return (range(1, self._num_rows + 1),
+                "ABCDEFGHJK"[:self._num_seats_per_row])
