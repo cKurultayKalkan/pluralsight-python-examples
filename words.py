f80759ff7dcdc88e1cc7d174b5974a6c4339eca8
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 09:27:50 +0300

    update words.py

--- words.py
+++ words.py
@@ -1,12 +1,16 @@
 from urllib.request import urlopen
 
 
-with urlopen('http://sixty-north.com/c/t.txt') as story:
-    story_words= []
-    for line in story:
-        line_words = line.decode('utf-8').split()
-        for word in line_words:
-            story_words.append(word)
+def fetch_words():
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
 
-for word in story_words:
-    print(word)
+    for word in story_words:
+        print(word)
+
+if __name__ == "__main__":
+    fetch_words()
