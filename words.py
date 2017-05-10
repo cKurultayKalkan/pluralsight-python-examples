f26babc058210c3164efba756d83f15b8ed8e8a2
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 09:25:34 +0300

    print word

--- words.py
+++ words.py
@@ -7,3 +7,6 @@ with urlopen('http://sixty-north.com/c/t.txt') as story:
         line_words = line.decode('utf-8').split()
         for word in line_words:
             story_words.append(word)
+
+for word in story_words:
+    print(word)
