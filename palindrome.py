fb289e6db86133ef38c0bfcd2ae70a4c0b4328f4
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 13:22:00 +0300

    add test cases to palindrome

--- palindrome.py
+++ palindrome.py
@@ -42,3 +42,24 @@ def is_palindrome(x):
         if f != r:
             return False
     return True
+
+
+class Tests(unittest.TestCase):
+    """Tests for the ``is_palindrome()`` function."""
+
+    def test_negative(self):
+        """Check that it returns False correctly."""
+        self.assertFalse(is_palindrome(1234))
+
+    def test_positive(self):
+        """Check that it returns True correctly."""
+        self.assertTrue(is_palindrome(1234321))
+
+    def test_single_digit(self):
+        """Check that it works for single digit numbers"""
+        for i in range(10):
+            self.assertTrue(is_palindrome(i))
+
+
+if __name__ == "__main__":
+    unittest.main()
