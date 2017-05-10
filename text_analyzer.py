fdcaff5e41aafcc516a620caa0b8d83cd5ccb89b
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 13:11:12 +0300

    complete text_analyzer

--- text_analyzer.py
+++ text_analyzer.py
@@ -3,7 +3,26 @@ import unittest
 
 
 def analyze_text(filename):
-    pass
+    """Calculate the number of lines and characters in a file
+
+    Args:
+        filename : The name of file to analyze
+
+    Raises:
+        IOError: If ``filename`` does not exist or can't be read.
+
+    Returns:
+        The number of lines in the file
+    """
+
+    lines = 0
+    chars = 0
+
+    with open(filename, 'r') as f:
+        for line in f:
+            lines += 1
+            chars += len(line)
+        return (lines, chars)
 
 
 class TextAnalysisTests(unittest.TestCase):
@@ -28,6 +47,25 @@ class TextAnalysisTests(unittest.TestCase):
     def test_function_runs(self):
         """Basic smoke test: does function run."""
         analyze_text(self.filename)
+
+    def test_line_count(self):
+        """Check that the line count is correct."""
+        self.assertEqual(analyze_text(self.filename)[0], 4)
+
+    def test_character_count(self):
+        """Check that the character count is correct."""
+
+        self.assertEqual(analyze_text(self.filename)[1], 131)
+
+    def test_no_such_file(self):
+        """Check the proper exception is thrown for a missing file."""
+        with self.assertRaises(IOError):
+            analyze_text('foobar')
+
+    def test_no_deletion(self):
+        """Check that the function doesn't delete the input file."""
+        analyze_text(self.filename)
+        self.assertTrue(os.path.exists(self.filename))
 
 
 if __name__ == "__main__":
