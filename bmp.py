454a5c17cb211f2775b0b6cfe6527390e1a35052
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 09:08:23 +0300

    add dimensions func

--- bmp.py
+++ bmp.py
@@ -1,6 +1,39 @@
 """"A module for dealing with BMP bitmap image files"""
 
 
+def _bytes_to_int32(b):
+    """Convert a bytes object containing four bytes into an integer."""
+    return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24)
+
+
+def dimensions(filename):
+    """Determine the dimensions in pixels of a BMP image.
+
+    Args:
+        filename : The file name of BMP File.
+
+    Returns:
+        A tuple containing two integers with the with
+        and height in pixels.
+
+    Raises:
+        ValueError: If the file was not  a BMP file.
+        OSError: If there was a problem reading the file.
+    """
+
+    with open(filename, 'rb') as f:
+        magic = f.read(2)
+        if magic != b'BM':
+            raise ValueError("{} is not a BMP file".format(filename))
+
+        f.seek(18)
+        width_bytes = f.read(4)
+        height_bytes = f.read(4)
+
+        return (_bytes_to_int32(width_bytes),
+                _bytes_to_int32(height_bytes))
+
+
 def write_grayscale(filename, pixels):
     """"Creates and writes a grayscale BMP file.
 
