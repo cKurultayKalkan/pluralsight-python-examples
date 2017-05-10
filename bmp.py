2600d54ac677f0eace306cccea51c18cc91ed802
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 08:27:45 +0300

    add _int_32_to_bytes func

--- bmp.py
+++ bmp.py
@@ -66,3 +66,11 @@ def write_grayscale(filename, pixels):
         # Fill in pixel offset placeholder
         bmp.seek(pixes_offset_bookmark)
         bmp.write(_int32_to_bytes(pixel_data_bookmark))
+
+
+def _int32_to_bytes(i):
+    """"Convert an integer to four bytes in little-endian format."""
+    return bytes((i & 0xff,
+                  i >> 8 & 0xff,
+                  i >> 16 & 0xff,
+                  i >> 32 & 0xff))
