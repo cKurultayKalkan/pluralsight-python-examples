ee92586fe8eb239229d799eef217231581e42788
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 07:38:37 +0300

    series py

---
+++ series.py
@@ -0,0 +1,22 @@
+""""Read and print an integer series"""
+
+import sys
+
+
+def read_series(filename):
+    f = open(filename, mode='rt', encoding='utf-8')
+    series = []
+    for line in f:
+        a = int(line.strip())
+        series.append(a)
+    f.close()
+    return series
+
+
+def main(filename):
+    series = read_series(filename)
+    print(series)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
