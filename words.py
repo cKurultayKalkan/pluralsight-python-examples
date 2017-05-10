7a38ea43b0fae70657b1ccbdd06c0b32f95e1f93
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 09:28:50 +0300

    update words.py

--- words.py
+++ words.py
@@ -8,9 +8,13 @@ def fetch_words():
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
+    return story_words
 
+
+def print_words(story_words):
     for word in story_words:
         print(word)
+
 
 if __name__ == "__main__":
     fetch_words()
