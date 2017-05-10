8b37909a080725f521c019951f9c2ce2c71db27f
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-10 08:17:36 +0300

    update bmp

--- bmp.py
+++ bmp.py
@@ -1,5 +1,6 @@
 """"A module for dealing with BMP bitmap image files"""
 
+
 def write_grayscale(filename, pixels):
     """"Creates and writes a grayscale BMP file.
 
@@ -18,18 +19,41 @@ def write_grayscale(filename, pixels):
     width = len(pixels[0])
 
     with open(filename, 'wb') as bmp:
-        #BMP Header
+        # BMP Header
         bmp.write(b'BM')
 
-        size_bookmark = bmp.tell()      # The next four btyes hold the filesize as a 32-bit
+        size_bookmark = bmp.tell()  # The next four btyes hold the filesize as a 32-bit
         bmp.write(b'\x00\x00\x00\x00')  # little-endian integer. Zero placeholder for now
 
         bmp.write(b'\x00\x00')  # unused 16-bit integer - should be zero
         bmp.write(b'\x00\x00')  # unused 16-bit integer - should be zero
 
-        pixes_offset_bookmark = bmp.tell()      # The next four bytes hold the integer offset
-        bmp.write(b'\x00\x00\x00\x00')          # to the pixel data. Zero placeholder for row.
+        pixes_offset_bookmark = bmp.tell()  # The next four bytes hold the integer offset
+        bmp.write(b'\x00\x00\x00\x00')  # to the pixel data. Zero placeholder for row.
 
-        #Image Header
-        bmp.write()
+        # Image Header
+        bmp.write(b'\x28\x00\x00\x00')  # Image header size in bytes - 40 decimal
+        bmp.write(_int_32_to_bytes(width))  # The image with in pixels
+        bmp.write(_int_32_to_bytes(height))  # The image height in pixels
+        bmp.write(b'\x01\x00')  # The number of image planes
+        bmp.write(b'\x08\x00')  # Bits per pixel 8 for grayscale
+        bmp.write(b'\x00\x00\x00\x00')  # No compression
+        bmp.write(b'\x00\x00\x00\x00')  # Zero for uncompressed images
+        bmp.write(b'\x00\x00\x00\x00')  # Unused pixels per meter
+        bmp.write(b'\x00\x00\x00\x00')  # Unused pixels per meter
+        bmp.write(b'\x00\x00\x00\x00')  # Use whole color table
+        bmp.write(b'\x00\x00\x00\x00')  # All colors are important
 
+        # Color palette - a linear grayscale
+        for c in range(0, 256):
+            bmp.write(bytes((c, c, c, 0)))  # Blue, Green, Red, Zero
+
+        # Pixel data
+        pixel_data_bookmark = bmp.tell()
+        for row in reversed(pixels):  # BMP files are bottom to top
+            row_data = bytes(row)
+            bmp.write(row_data)
+            padding = b'\x00' * (4 - (len(row) % 4))  # Pad row to multiple of four bytes
+            bmp.write(padding)
+
+        # End of file
